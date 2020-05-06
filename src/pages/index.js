7d5dc22621be6dfab6f44f9686837b45d3e138e5
dexmon <dexmon@example.com>
2020-05-06 18:39:12 +0900

    テストページを作成する

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,14 @@
 import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
 
-export default () => <div>Hello world!</div>
+export default () => {
+  return (
+    <div style={{ color: "teal" }}>
+      <Link to="/">Home</Link>
+      <Header />
+      <p>テストページ</p>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
+    </div>
+  )
+}
