1f853022c6e6f5bfac71fecdfb645ded7f068ef9
Tomoya Sunda <dxm.issatisfied@gmail.com>
2020-05-11 21:54:40 +0900

    wip

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,38 @@ import { Link } from "gatsby"
 import Header from "../components/header"
 
 export default () => {
+  const [value, setValue] = React.useState(0)
+
+  const [value2, setValue2] = React.useState(0)
+
+  const [value3, setValue3] = React.useState(0)
+
+  const handleChange = event => {
+    setValue(Number(event.target.value))
+  }
+
+  const handleChange2 = event => {
+    setValue2(Number(event.target.value))
+  }
+
+  const handleChange3 = event => {
+    setValue3(Number(event.target.value))
+  }
+
   return (
     <div style={{ color: "teal" }}>
       <Link to="/">Home</Link>
       <Header />
       <p>テストページ</p>
       <img src="https://source.unsplash.com/random/400x200" alt="" />
+      <input
+        type="number"
+        value={String(value)}
+        required
+        onChange={handleChange}
+      />
+      <input type="number" value={String(value2)} onChange={handleChange2} />
+      <input type="number" value={value3} onChange={handleChange3} />
     </div>
   )
 }
